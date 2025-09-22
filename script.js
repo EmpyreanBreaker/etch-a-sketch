c4bc9bfbb01909183f69d5dfe4b925932372e3de
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2025-09-22 16:55:29 -0500

    feat: adds index.html, script.js, and styles.css

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ * User clicks a button
+ * Button brings up an input
+ * User puts the input which creates a grid of that certain size
+ * Input must be less than 100
+ */
+
+
+/**
+ * STEP ONE
+ * Grab container div
+ * Create a div element
+ * Loop to create divs of a certain amount
+ * Style the div so it shows
+ * Use Flexbos to change positioning
+ * Flex warp maybe
+ */
+
+

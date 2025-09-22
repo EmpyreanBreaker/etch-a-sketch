456189d19200d9640e0a20afcca9fcb4e83cdfed
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2025-09-22 18:18:56 -0500

    feat: finish cell build logic

--- script.js
+++ script.js
@@ -6,14 +6,23 @@
  */
 
 
-/**
- * STEP ONE
- * Grab container div
- * Create a div element
- * Loop to create divs of a certain amount
- * Style the div so it shows
- * Use Flexbos to change positioning
- * Flex warp maybe
- */
+// Grab Container Div
+const container = document.querySelector(".etch__container");
 
+function createBackground(size) {
+    // Basis is the starting width/height of each item in the flex space depending on the flex direction
+    // We want each item to take only a certain percentage of space for accuracy rather than hard values
+    const basis = 100 / size;
 
+    // Create Cell Element - Give it a class - Loop & append it to the container element
+    // If the user enters 16 then you want a 16 by 16 grid so it's 16 squared total which is 256
+    for (let i = 0; i < size * size; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("etch__cell");
+        cell.style.flex = `0 0 ${basis}%`;
+        cell.style.aspectRatio = '1/1';
+        container.appendChild(cell);
+    }
+}
+
+createBackground(10);
